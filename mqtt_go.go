da4c5b5548cade77a41df4543fd94c515620d099
root <root@localhost.localdomain>
2022-11-10 04:35:38 -0500

    Optimize code

--- mqtt_go.go
+++ mqtt_go.go
@@ -22,18 +22,19 @@ var connectionLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, 
 
 func main() {
 	broker := flag.String("broker", "tcp://app0.pandavon.com:1883", "The broker URI. ex: tcp://10.10.1.1:1883")
-	topic := flag.String("topic", "liulei/test", "The topic name to/from which to publish/subscribe")
+	topic := flag.String("topic", "", "The topic name to/from which to publish/subscribe")
 	password := flag.String("password", "", "The password (optional)")
 	user := flag.String("user", "", "The User (optional)")
 	id := flag.String("id", "testgoid", "The ClientID (optional)")
 	cleansess := flag.Bool("clean", false, "Set Clean Session (default false)")
-	qos := flag.Int("qos", 0, "The Quality of Service 0,1,2 (default 0)")
+	qos := flag.Int("qos", 1, "The Quality of Service 0,1,2 (default 0)")
 	payload := flag.String("message", "", "The message text to publish (default empty)")
 	store := flag.String("store", ":memory:", "The Store Directory (default use memory store)")
 	flag.Parse()
 
 	if *topic == "" {
 		fmt.Println("Invalid setting for -topic, must not be empty")
+		fmt.Println("./mqtt_go -broker tcp://10.10.1.1:1883 -user xxx -password xxx -topic xxx")
 		return
 	}
 
@@ -58,24 +59,34 @@ func main() {
 	options.OnConnect = connectHandler
 	options.OnConnectionLost = connectionLostHandler
 
+	options.SetCleanSession(*cleansess)
+	//options.SetKeepAlive(60)
+	////自动重连机制，如网络不稳定可开启
+	options.SetAutoReconnect(true)      //启用自动重连功能
+	options.SetMaxReconnectInterval(30) //每30秒尝试重连
+	if *store != ":memory:" {
+		options.SetStore(mqtt.NewFileStore(*store))
+	}
+
 	client := mqtt.NewClient(options)
 	token := client.Connect()
 	if token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	//topic := "topic/secret"
-	token = client.Subscribe(*topic, 1, nil)
+	topic_sub := "liulei/rpc"
+	token = client.Subscribe(topic_sub, byte(*qos), nil)
 	token.Wait()
-	fmt.Printf("Subscribed to topic %s\n", *topic)
+	fmt.Printf("Subscribed to topic %s\n", topic_sub)
 
 	num := 10
 	for i := 0; i < num; i++ {
 		text := fmt.Sprintf("%d", i)
-		token = client.Publish(*topic, 0, false, text)
+		token = client.Publish(*topic, byte(*qos), false, text)
 		token.Wait()
 		time.Sleep(time.Second)
 	}
+	client.Unsubscribe(topic_sub)
 
 	client.Disconnect(100)
 }
